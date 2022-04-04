66486d20095df19ebebf8beb421c900338859b2b
danielgundersen09 <dangun-09@hotmail.com>
2022-04-04 23:45:27 +0200

    Updates

---
+++ precache-manifest.ba4221133d2c49624c3b7c5821b39f03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d98244eb0df4c71dd6bc0520cc1c5db",
+    "url": "/Portfolio-website/index.html"
+  },
+  {
+    "revision": "0eebcca4d19fc3ca0f4a",
+    "url": "/Portfolio-website/static/css/2.0547ff38.chunk.css"
+  },
+  {
+    "revision": "077d227284d132398420",
+    "url": "/Portfolio-website/static/css/main.eaa9b092.chunk.css"
+  },
+  {
+    "revision": "0eebcca4d19fc3ca0f4a",
+    "url": "/Portfolio-website/static/js/2.52038344.chunk.js"
+  },
+  {
+    "revision": "c003df59d0b5e92ff77cb6d426eaf40c",
+    "url": "/Portfolio-website/static/js/2.52038344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077d227284d132398420",
+    "url": "/Portfolio-website/static/js/main.a96f57c9.chunk.js"
+  },
+  {
+    "revision": "119ae820c45fd539374d",
+    "url": "/Portfolio-website/static/js/runtime-main.3ebbd5d2.js"
+  }
+]);
